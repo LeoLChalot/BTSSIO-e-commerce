a6f9fd05a75a2e2ee90912a9afcf531ea2bb8f59
LeoLChalot <larouchalotleo@gmail.com>
2023-01-11 12:12:25 +0100

    opti

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php require_once(__DIR__ . '/includes/bddConn.php'); ?>
 <?php require_once(__DIR__ . '/includes/header.php'); ?>
-<?php
-$sth = $conn->prepare("SELECT * FROM categorie");
-$sth->execute();
-$fetch = $sth->fetchAll(PDO::FETCH_ASSOC);
-var_dump($fetch);
-for ($i = 0; $i < count($fetch); $i++) : ?>
-    <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
-
-        <!-- Photo Grid-->
-        <div class="w3-row-padding w3-padding-16 w3-center">
-
-            <?php var_dump($fetch[$i]); ?>
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+    <!-- Photo Grid-->
+    <div class="w3-row-padding w3-padding-16 w3-center">
+        <?php
+        $sth = $conn->prepare("SELECT * FROM categorie");
+        $sth->execute();
+        $fetch = $sth->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($fetch);
+        for ($i = 0; $i < count($fetch); $i++) : ?>
             <div class="w3-quarter">
                 <img class="img-card" src="assets/img/categorie/<?= $fetch[$i]['image'] ?>" alt="Popsicle" style="width:100%">
                 <h3>
@@ -22,8 +19,8 @@ for ($i = 0; $i < count($fetch); $i++) : ?>
                 </h3>
             </div>
         <?php endfor;  ?>
-        </div>
     </div>
-    <?php require_once(__DIR__ . '/includes/pagination.php'); ?>
-    <?php require_once(__DIR__ . '/includes/footer.php'); ?>
-    <?php require_once(__DIR__ . '/includes/bddOff.php'); ?>
+</div>
+<?php require_once(__DIR__ . '/includes/pagination.php'); ?>
+<?php require_once(__DIR__ . '/includes/footer.php'); ?>
+<?php require_once(__DIR__ . '/includes/bddOff.php'); ?>
--- login.php
+++ login.php
@@ -3,15 +3,11 @@
 <!-- !PAGE CONTENT! -->
 <div class="w3-display-middle w3-padding w3-col 8 m4">
     <form class="w3-container" action="login.php" method="POST">
-
         <label class="w3-text-blue"><b>Pseudo</b></label>
         <input class="w3-input w3-border w3-margin" for="pseudo" type="text" required>
-
         <label class="w3-text-blue"><b>Mot de passe</b></label>
         <input class="w3-input w3-border w3-margin" for="pwd" type="password" required>
-
         <button class="w3-btn w3-blue w3-margin" type="submit">login</button>
-
     </form>
 </div>
 
@@ -19,12 +15,10 @@
 if (isset($_POST['submit'])) {
     $pseudo = $_POST['pseudo'];
     $pwd = $_POST['pwd'];
-
     // check if user is registered
     $sth = $conn->prepare("SELECT * FROM user WHERE pseudo = $pseudo AND pwd = $pwd");
     $sth->execute();
     $fetch = $sth->fetchAll(PDO::FETCH_ASSOC);
-
     if (count($fetch) > 0) {
         header('Location:index.php');
     } else {
