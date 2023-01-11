10aa48896b925dfadcd8c0fcd0c096d1f40d0944
LeoLChalot <larouchalotleo@gmail.com>
2023-01-11 17:05:25 +0100

    Léo updt

--- includes/header.php
+++ includes/header.php
@@ -15,11 +15,7 @@
     <nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
         <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button">Close Menu</a>
         <a href="index.php" onclick="w3_close()" class="w3-bar-item w3-button">Home</a>
-<<<<<<< HEAD
         <a href="items.php" onclick="w3_close()" class="w3-bar-item w3-button">All Items</a>
-=======
-        <a href="item.php" onclick="w3_close()" class="w3-bar-item w3-button">All Items</a>
->>>>>>> a6f9fd05a75a2e2ee90912a9afcf531ea2bb8f59
         <a href="login.php" onclick="w3_close()" class="w3-bar-item w3-button">Login</a>
     </nav>
 
